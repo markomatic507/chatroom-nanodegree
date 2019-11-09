cc5cb0ba611c998d7bd6a68418a0bd79544d7f76
markomatic507 <marko.matic507@gmail.com>
2019-11-09 21:55:13 +0100

    Added sending messages, open connection, send message and, close connection

--- src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
+++ src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
@@ -1,5 +1,6 @@
 package edu.udacity.java.nano.chat;
 
+import com.alibaba.fastjson.JSON;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
@@ -44,7 +45,7 @@ public class WebSocketChatServer {
     public void onOpen(Session session, @PathParam("username") String username) {
         onlineSessions.put(session.getId(), session);
         usernames.put(session.getId(), username);
-        sendMessageToAll(Message.Json("Entered the chat", username, onlineSessions.size(), "ENTER"));
+        sendMessageToAll(Message.Json(username, "Entered the chat", onlineSessions.size(), "ENTER"));
     }
 
     /**
@@ -52,7 +53,8 @@ public class WebSocketChatServer {
      */
     @OnMessage
     public void onMessage(Session session, String jsonStr) {
-        //TODO: add send message.
+        Message message = JSON.parseObject(jsonStr, Message.class);
+        sendMessageToAll(Message.Json(message.getUsername(), message.getMsg(), onlineSessions.size(), "SPEAK"));
     }
 
     /**
@@ -60,7 +62,9 @@ public class WebSocketChatServer {
      */
     @OnClose
     public void onClose(Session session) {
-        //TODO: add close connection.
+        sendMessageToAll(Message.Json(usernames.get(session.getId()), "Left the chat", onlineSessions.size(), "LEAVE"));
+        usernames.remove(session.getId());
+        onlineSessions.remove(session.getId());
     }
 
     /**
