46b6ca8b5349e7eb930e305a5f78963248c2e10a
markomatic507 <marko.matic507@gmail.com>
2019-11-09 21:48:02 +0100

    Added message model

--- src/main/java/edu/udacity/java/nano/chat/Message.java
+++ src/main/java/edu/udacity/java/nano/chat/Message.java
@@ -1,8 +1,56 @@
 package edu.udacity.java.nano.chat;
 
+import com.alibaba.fastjson.JSON;
+
 /**
  * WebSocket message model
  */
 public class Message {
-    // TODO: add message model.
+    private String username;
+    private String msg;
+    private int onlineCount;
+    private String type;
+
+    public Message(String username, String msg, int onlineCount, String type) {
+        this.username = username;
+        this.msg = msg;
+        this.onlineCount = onlineCount;
+        this.type = type;
+    }
+
+    public static String Json(String username, String msg, int onlineCount, String type) {
+        return JSON.toJSONString(new Message(username, msg, onlineCount, type));
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public int getOnlineCount() {
+        return onlineCount;
+    }
+
+    public void setOnlineCount(int onlineCount) {
+        this.onlineCount = onlineCount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
--- src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
+++ src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
@@ -3,7 +3,10 @@ package edu.udacity.java.nano.chat;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -21,18 +24,27 @@ public class WebSocketChatServer {
     /**
      * All chat sessions.
      */
+    private static HashMap<String, String> usernames = new HashMap<>();
     private static Map<String, Session> onlineSessions = new ConcurrentHashMap<>();
 
     private static void sendMessageToAll(String msg) {
-        //TODO: add send message method.
+        onlineSessions.forEach((id, session) -> {
+            try {
+                session.getBasicRemote().sendText(msg);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     /**
      * Open connection, 1) add session, 2) add user.
      */
     @OnOpen
-    public void onOpen(Session session) {
-        //TODO: add on open connection.
+    public void onOpen(Session session, @PathParam("username") String username) {
+        onlineSessions.put(session.getId(), session);
+        usernames.put(session.getId(), username);
+        sendMessageToAll(Message.Json("Entered the chat", username, onlineSessions.size(), "ENTER"));
     }
 
     /**
